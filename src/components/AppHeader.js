b3695a321eea6b9c5e94d1f40ad919bd49a5a65b
Hülya Nergiz <akin.hulya@outlook.com>
2023-11-21 21:56:32 +0300

    gets favorites and displayFavorites from store

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 
 const AppHeader = (props) => {
   const appTitle = useSelector((store) => store.appTitle);
-  const displayFavorites = true;
+  const displayFavorites = useSelector(
+    (store) => store.favoritesReducer.displayFavorites
+  );
 
   return (
     <div className="flex justify-between items-center shadow rounded-md bg-white p-2 pl-3 my-3">
