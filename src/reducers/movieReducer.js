5876f20f202f01d5a76cdb48e883ba7ff52446f6
Hülya Nergiz <akin.hulya@outlook.com>
2023-11-21 21:21:22 +0300

    adds a new movie to movies

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -13,6 +13,8 @@ const reducer = (state = initialState, action) => {
         ...state,
         movies: state.movies.filter((item) => action.payload !== item.id),
       };
+    case ADD_MOVIE:
+      return { ...state, movies: [...state.movies, action.payload] };
     default:
       return state;
   }
