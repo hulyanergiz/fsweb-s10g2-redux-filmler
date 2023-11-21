26bb4e708c7f8c6b9d237cd1620fd466368a82ed
Hülya Nergiz <akin.hulya@outlook.com>
2023-11-21 21:44:19 +0300

    combineReducers (movies&favorites)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
+import favoritesReducer from "./favoritesReducer";
+import movieReducer from "./movieReducer";
 
-import movieReducer from './movieReducer';
-
-export default movieReducer;
+export const reducers = combineReducers({
+  movieReducer,
+  favoritesReducer,
+});
+export default reducers;
